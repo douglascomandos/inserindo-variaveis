ed2dde4400e0321dc1ea8b62568c905f459d9921
DOUGLAS ROGERIO ALMEIDA SILVA <douglas.rasilva1@senacsp.edu.br>
2025-10-22 21:47:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// comentario 
+//console.WhiteLIne("qualquerr coisa");
+
+//VARIAVEIS E TIPOS 
+
+var n = 10;
+
+int x = 1;
+double preco = 10.50;
+char inicial = 'D';
+string nome = "Douglas";
+bool status = true;
+int idade = 31;
+
+Console.WriteLine($"nome: {nome}");
+Console.WriteLine($"idade: {idade}");
+Console.WriteLine($"Status: {status}");
